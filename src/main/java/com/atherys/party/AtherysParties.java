2a535b6d7d010a9b60b2695e6d7c4715154ac4ca
Miroslav Vasilev <mvvasilev@outlook.com>
2019-12-04 12:35:58 +0200

    Update AtherysParties.java

--- src/main/java/com/atherys/party/AtherysParties.java
+++ src/main/java/com/atherys/party/AtherysParties.java
@@ -33,7 +33,7 @@ public class AtherysParties {
     public static final String ID = "atherysparties";
     public static final String NAME = "A'therys Parties";
     public static final String DESCRIPTION = "A Party plugin for the A'therys Horizons server";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "%PLUGIN_VERSION%";
 
     private static AtherysParties instance;
 
